208cf079a1f3dc3ac89eb00452e744f11c182b12
Itagon226 <longvongola@gmail.com>
2025-09-27 09:03:48 +0700

    HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Đỗ Đức Long\t24021550\tINT2204 6\tdolong226\t24021550@edu.vnu.vn");
+        for(int i = 9; i >= 1; i--) {
+            System.out.println(i+" bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around.");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
